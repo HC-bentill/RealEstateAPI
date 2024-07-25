fe22bb67648670a3f02ee4e148f415ade0d61a78
Henry Bentil <hbentil@acsint.io>
2024-07-25 10:44:00 +0000

    first commit

--- obj/Debug/net8.0/RealEstateAPI.AssemblyInfo.cs
+++ obj/Debug/net8.0/RealEstateAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RealEstateAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c7fd916c6cdc92dc5eae6c8293b8a27646b668aa")]
 [assembly: System.Reflection.AssemblyProductAttribute("RealEstateAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RealEstateAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
